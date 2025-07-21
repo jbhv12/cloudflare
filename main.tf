dcc33cadf5476f69e615dd6fac6543a1be91940a
Jay Bhavsar <jay@local>
2025-07-21 19:34:02 +0530

    zoho mail

--- main.tf
+++ main.tf
@@ -15,9 +15,44 @@ provider "cloudflare" {}
 # }
 
 resource "cloudflare_record" "oracle" {
-  zone_id = "930ad404e3042fe25ab656fd3a2f3927"
+  zone_id = var.dns_zone_id
   name    = "oracle"
   value   = "152.67.2.209"
   type    = "A"
   proxied = false
+}
+
+resource "cloudflare_record" "mx_jay10" {
+  zone_id  = var.dns_zone_id
+  name     = "@"
+  value    = "jay10mx.zoho.in"
+  type     = "MX"
+  priority = 10
+  ttl      = 300
+}
+
+resource "cloudflare_record" "mx_jay20" {
+  zone_id  = var.dns_zone_id
+  name     = "@"
+  value    = "jay20mx2.zoho.in"
+  type     = "MX"
+  priority = 20
+  ttl      = 300
+}
+
+resource "cloudflare_record" "mx_jay50" {
+  zone_id  = var.dns_zone_id
+  name     = "@"
+  value    = "jay50mx3.zoho.in"
+  type     = "MX"
+  priority = 50
+  ttl      = 300
+}
+
+resource "cloudflare_record" "spf_record" {
+  zone_id = var.dns_zone_id
+  name    = "@"
+  value   = "v=spf1 include:zoho.in ~all"
+  type    = "TXT"
+  ttl     = 300
 }
--- variables.tf
+++ variables.tf
@@ -3,7 +3,11 @@ variable "cloudflare_account_id" {
   type        = string
   default     = "d8323c6cfda50c6ff2a74c6e6a01f1f3"
 }
-
+variable "dns_zone_id" {
+  description = "The domain name to manage."
+  type        = string
+  default     = "930ad404e3042fe25ab656fd3a2f3927"
+}
 variable "domain_name" {
   description = "The domain name to manage."
   type        = string
