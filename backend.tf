a74f3c0f427e9d477da552ecf7865e13b45742d2
Jay Bhavsar <jay@local>
2025-10-21 19:47:47 +0530

    trimspace

--- backend.tf
+++ backend.tf
@@ -25,6 +25,6 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key = var.private_key
+  private_key = trimspace(replace(var.private_key, "\r\n", "\n"))
   region           = var.region
 }
