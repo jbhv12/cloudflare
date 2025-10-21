f285556258d13a9248a44f36c97a14cf83577328
Jay Bhavsar <jay@local>
2025-10-21 21:19:45 +0530

    vm2

--- oci.tf
+++ oci.tf
@@ -1,37 +1,53 @@
+variable "ssh_public_key" {
+  type = string
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1cPNrK+VUGznUjnAJVtj2yA4ChyuQYz/J42rVfAtKG jay@laptop"
+}
+
 data "oci_identity_tenancy" "tenancy" {
   tenancy_id = var.tenancy_ocid
 }
-data "oci_identity_compartments" "compartments" {
+
+data "oci_identity_availability_domains" "ads" {
   compartment_id = data.oci_identity_tenancy.tenancy.id
-  access_level    = "ANY"
-  compartment_id_in_subtree = true
 }
+
+locals {
+  root_compartment_ocid = data.oci_identity_tenancy.tenancy.id
+  ad1_name              = one([for ad in data.oci_identity_availability_domains.ads.availability_domains : ad.name if length(regexall("AD-1$", ad.name)) > 0])
+}
+
 data "oci_core_images" "ubuntu_latest" {
   compartment_id           = local.root_compartment_ocid
   operating_system         = "Canonical Ubuntu"
   operating_system_version = "24.04"
-  shape                    = "VM.Standard3.Flex"
-
-  sort_by    = "TIMECREATED"
-  sort_order = "DESC"
-}
-locals {
-  root_compartment_ocid = data.oci_identity_tenancy.tenancy.id
+  shape                    = "VM.Standard.E2.1.Micro"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
 }
 
 resource "oci_core_virtual_network" "vcn" {
   cidr_block     = "10.0.0.0/16"
   compartment_id = local.root_compartment_ocid
   display_name   = "example-vcn"
+  dns_label      = "examplevcn"
 }
 
-resource "oci_core_subnet" "subnet" {
-  cidr_block          = "10.0.1.0/24"
-  compartment_id      = local.root_compartment_ocid
-  vcn_id              = oci_core_virtual_network.vcn.id
-  display_name        = "example-subnet"
-  availability_domain = "AD-1"
-  prohibit_public_ip_on_vnic = false
+resource "oci_core_internet_gateway" "igw" {
+  compartment_id = local.root_compartment_ocid
+  vcn_id         = oci_core_virtual_network.vcn.id
+  display_name   = "example-igw"
+  enabled        = true
+}
+
+resource "oci_core_route_table" "rt" {
+  compartment_id = local.root_compartment_ocid
+  vcn_id         = oci_core_virtual_network.vcn.id
+  display_name   = "example-rt"
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.igw.id
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
 }
 
 resource "oci_core_security_list" "sec_list" {
@@ -40,25 +56,31 @@ resource "oci_core_security_list" "sec_list" {
   display_name   = "example-sec-list"
 
   ingress_security_rules {
-    protocol = "6" # TCP
-    source   = "0.0.0.0/0"
-
-    tcp_options {
-      min = 22
-      max = 22
-    }
+    protocol    = "all"
+    source = "0.0.0.0/0"
   }
 
   egress_security_rules {
-    protocol = "all"
+    protocol    = "all"
     destination = "0.0.0.0/0"
   }
 }
 
+resource "oci_core_subnet" "subnet" {
+  cidr_block                 = "10.0.1.0/24"
+  compartment_id             = local.root_compartment_ocid
+  vcn_id                     = oci_core_virtual_network.vcn.id
+  display_name               = "example-subnet"
+  prohibit_public_ip_on_vnic = false
+  route_table_id             = oci_core_route_table.rt.id
+  security_list_ids          = [oci_core_security_list.sec_list.id]
+  dns_label                  = "examplesubnet"
+}
+
 resource "oci_core_instance" "vm_instance" {
   compartment_id      = local.root_compartment_ocid
-  availability_domain = "AD-1"
-  shape               = "VM.Standard3.Flex"
+  availability_domain = local.ad1_name
+  shape               = "VM.Standard.E2.1.Micro"
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.subnet.id
@@ -68,7 +90,11 @@ resource "oci_core_instance" "vm_instance" {
 
   source_details {
     source_type = "image"
-    source_id    = data.oci_core_images.ubuntu_latest.images[0].id
+    source_id   = data.oci_core_images.ubuntu_latest.images[0].id
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
   }
 
   display_name = "example-instance"
